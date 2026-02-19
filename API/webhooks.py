728d3eb9fa13a610bdc34ce01f5b315d6cd80685
Grigs04 <meizu2mini10@gmail.com>
2026-02-19 13:13:07 +0300

    a

--- API/webhooks.py
+++ API/webhooks.py
@@ -4,30 +4,20 @@ from fastapi import APIRouter, Request, HTTPException
 
 router = APIRouter()
 
-# -------------------- LOGGING --------------------
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# -------------------- HEALTHCHECK --------------------
-
 @router.get("/ping")
 async def ping():
     return {"status": "ok"}
 
-# -------------------- AMOCRM WEBHOOK --------------------
 
 @router.post("/amo/create-invoice")
 async def create_invoice_from_amo(request: Request):
-    form = await request.form()
-    data = dict(form)
-    lead_id = data.get("leads[add][0][id]")
-
-
-    logger.info("📩 Webhook from amoCRM received")
-    logger.info(data)
+    raw_data = await request.body()
+    data = json.loads(raw_data)
+    with open('body.json', 'w') as f:
+        json.dump(data, f)
 
     return {
         "status": "ok",
         "message": "Webhook received"
     }
+
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import logging
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from API.webhooks import router as webhook_router
 
-# -------------------- LOGGING --------------------
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# -------------------- LIFESPAN --------------------
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    logger.info("🚀 Application started")
-    yield
-    logger.info("🛑 Application stopped")
-
-# -------------------- APP --------------------
-
-app = FastAPI(lifespan=lifespan)
-
+app = FastAPI()
 app.include_router(webhook_router)
