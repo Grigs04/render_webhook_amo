5162b674df1e3be6a42134a6a4fb1135f8a01c9d
Alexander Grigoriev <Alexander.Grigoriev@icmr.ru>
2026-02-24 12:56:28 +0300

    l

--- API/webhooks.py
+++ API/webhooks.py
@@ -10,18 +10,19 @@ async def ping():
 
 @router.post("/amo/create-invoice")
 async def create_invoice_from_amo(request: Request):
-    data = await request.json()
-    try:
-        entity_id = data.get('data', [{}])[0].get('entity_id')
-        account_id = data.get('account_id')
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    await runner(entity_id)
-
-    return {
-        "status": "ok",
-        "message": "Webhook received",
-        "entity_id": entity_id
-    }
+    print(request.body)
+    # data = await request.json()
+    # try:
+    #     entity_id = data.get('data', [{}])[0].get('entity_id')
+    #     account_id = data.get('account_id')
+    # except Exception as e:
+    #     raise HTTPException(status_code=400, detail=str(e))
+    #
+    # await runner(entity_id)
+    #
+    # return {
+    #     "status": "ok",
+    #     "message": "Webhook received",
+    #     "entity_id": entity_id
+    # }
 
