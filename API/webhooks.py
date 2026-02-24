b240ef874a8fb1ed90a6c500955085de94a2e582
Alexander Grigoriev <Alexander.Grigoriev@icmr.ru>
2026-02-24 14:14:18 +0300

    ну ща вроде точно готово, пофиксил принятие формы запроса

--- API/webhooks.py
+++ API/webhooks.py
@@ -10,18 +10,16 @@ async def ping():
 
 @router.post("/amo/create-invoice")
 async def create_invoice_from_amo(request: Request):
-    data = await request.form()
-    print(data)
-    # account = json.loads(data.get('account'))
-    # lead = json.loads(data.get('leads'))
-    # entity_id = lead.get('add')[0].get('id')
+    form = await request.form()
+
+    entity_id = int(form.get('leads[add][0][id]'))
 
 
-    # await runner(entity_id)
-    #
-    # return {
-    #     "status": "ok",
-    #     "message": "Webhook received",
-    #     "entity_id": entity_id
-    # }
+    await runner(entity_id)
+
+    return {
+        "status": "ok",
+        "message": "Webhook received",
+        "entity_id": entity_id
+    }
 
